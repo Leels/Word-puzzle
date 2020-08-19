a241cc5ac58687056162b86d059cb8ab979ce84c
Lela Smith <smith.lelaa@gmail.com>
2020-08-18 19:52:00 -0700

    tidy up code example

--- js/scripts.js
+++ js/scripts.js
@@ -2,39 +2,36 @@ $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
 
-    //Business Logic
+    const puzzleInput = $("input#puzzleString").val();
+    const puzzleArray = puzzleInput.toLowerCase().split("")
 
-    var puzzles = $("input#puzzleString").val();
+    function replaceVowels() {
+      let newArray = []
 
-
-
-    //User Interface
-// THIS IS THE SPLIT/JOIN METHOD OF REPLACING CHARACTERS IN STRING //
-
-    if (puzzles.length === 0){
-      alert("Please enter text for a puzzle")
-    } else {
-      for ( let i = 0; i < puzzles.length; i ++) {
-        var tokens = puzzles.split('a').join("-").split('e').join("-").split('i').join('-').split('o').join('-').split('u').join("-");
-        console.log(tokens);
-        $(".generator").hide();
-        $(".result").text(tokens);
+      if (puzzleInput.length === 0){
+        alert("Please enter a sentence!")
+      } else {
+        for (let i = 0; i < puzzleArray.length; i ++) {
+          if (puzzleArray[i].includes("a") || puzzleArray[i].includes("e") || puzzleArray[i].includes("i") || puzzleArray[i].includes("o") || puzzleArray[i].includes("u")) {
+            newArray.push("-")
+          } else {
+            newArray.push(puzzleArray[i])
+          }
+        }
       }
+        return newArray.join(" ")
     };
 
+    $(".generator").hide();
+    $(".result").text(replaceVowels(puzzleInput));
 
-// THIS IS THE REGEX WAY OF REPLACING CHARACTERS IN STRING//
+    });
+  });
 
-   // for ( let i = 0; i < puzzles.length; i ++) {
-   //  var tokens = puzzles.replace(/a|e|i|o|u/g, "-");
-   //   console.log(tokens);
-   //   $(".generator").hide();
-   //   $(".result").text(tokens);
-   // }
+// THIS IS THE REGEX WAY OF REPLACING CHARACTERS IN STRING
+  //   const result = puzzleInput.replace(/a|e|i|o|u/g, "-");
+  //    $(".generator").hide();
+  //    $(".result").text(tokens);
+  //   });
+  // });
 
-
-
-
-});
-
-});
